1b720ac1589a0e378297669facd0212dada6de6f
Daniel Brooker <dan@nocturnalcode.com>
2015-03-16 15:11:14 +1300

    Initial Commit

---
+++ Data/Data.h
@@ -0,0 +1,19 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Daniel Brooker on 16/03/15.
+//  Copyright (c) 2015 Nocturnal Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
